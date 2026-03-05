05d247c9f3ac98e4af071d7e9aaa9c810d1535b8
StudioLee-cmd <tim@studiolee.nl>
2026-03-05 01:02:58 +0100

    Fix siteDetails: niche field, live siteUrl, siteName, language nl-NL; FreeTrialModal uses niche

--- src/data/siteDetails.ts
+++ src/data/siteDetails.ts
@@ -1,11 +1,12 @@
 export const siteDetails = {
     siteName: 'ReparatieAI',
-    siteUrl: 'https://reparatieai.nl/',
+    niche: 'Reparatiebedrijven',
+    siteUrl: 'https://www.reparatieai.nl/',
     metadata: {
         title: 'ReparatieAI - De slimme assistent voor jouw reparatiebedrijf',
         description: 'Bespaar zeeën van tijd en zeg vaarwel tegen saai kantoorwerk. ReparatieAI regelt je agenda, telefoon en administratie volledig automatisch.',
     },
-    language: 'nl-nl',
+    language: 'nl-NL',
     locale: 'nl-NL',
     siteLogo: '/images/logo-transparent.png',
     googleAnalyticsId: '', // e.g. G-XXXXXXX,
